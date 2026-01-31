dc2d43563d8a1337cf984a286bea12ed7c79baaa
programmertop5 <kardrkarina@gmail.com>
2026-01-31 16:04:40 +0300

    Update TeachersQuery.sql

--- TeachersQuery.sql
+++ TeachersQuery.sql
@@ -1,9 +1,12 @@
-﻿CREATE TABLE Teachers(
+CREATE TABLE Teachers(
 	[Id]  INT NOT NULL PRIMARY KEY IDENTITY,
-	[EmploymentDate]  DATE  NOT NULL,
+	[EmploymentDate]  DATE  NOT NULL
+	CHECK (EmploymentDate >= '1990-01-01'),
 	[Name]  NVARCHAR(MAX)  NOT NULL,
-	[Premium]  MONEY  NOT NULL DEFAULT 0,
-	[Salary]  MONEY  NOT NULL,
+	[Premium]  MONEY  NOT NULL DEFAULT 0
+	CHECK (Premium >= 0),
+	[Salary]  MONEY  NOT NULL
+	CHECK(Salary <= 0),
 	[Surname]  NVARCHAR(MAX)  NOT NULL
 )
 
